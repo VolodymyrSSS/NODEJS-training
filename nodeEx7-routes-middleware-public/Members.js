9fc740ad0fd83de23c9ff775911268741a203e44
Volodymyr <wolodymyrs@gmail.com>
2020-10-30 22:40:10 +0200

    Middleware routes views

---
+++ nodeEx7-routes-middleware-public/Members.js
@@ -0,0 +1,21 @@
+const members = [{
+    id: 1,
+    name: 'Zarathustra Samuel',
+    email: 'zarik @gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Carol Dingle',
+    email: 'dingg@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Bob Kudyba',
+    email: 'bkudyba@gmail.com',
+    status: 'active'
+  }
+]
+
+module.exports = members;
