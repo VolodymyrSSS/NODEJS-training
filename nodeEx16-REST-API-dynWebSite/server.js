8dcb59de52fd44a347214b99c4e17084a1d3147c
Volodymyr <wolodymyrs@gmail.com>
2020-11-08 13:01:18 +0200

    express is serving html and static files

--- nodeEx16-REST-API-dynWebSite/server.js
+++ nodeEx16-REST-API-dynWebSite/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
 const app = express();
+
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+// apply a middleware to look express into a static folder
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-  response.send('Hello Express :)');
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(PORT, () => {
